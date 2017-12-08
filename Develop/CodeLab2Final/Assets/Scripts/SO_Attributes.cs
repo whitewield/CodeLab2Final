ab83cf3106c6008f193f97f47ae58a47d7d5c03d
Hang <whitewield@126.com>
2017-12-08 15:18:24 -0500

    + global + attributes

---
+++ Develop/CodeLab2Final/Assets/Scripts/SO_Attributes.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "Attributes", menuName = "CodeLab2/Attributes", order = 1)]
+public class SO_Attributes : ScriptableObject {
+	/// <summary>
+	/// Hit Point.
+	/// </summary>
+	public int HP = 8;
+
+	/// <summary>
+	/// Damage.
+	/// </summary>
+	public int DMG = 2;
+
+	/// <summary>
+	/// Accuracy
+	/// </summary>
+	public float ACC = 0.8f;
+
+	/// <summary>
+	/// Cool Down.
+	/// </summary>
+	public float CD = 3;
+}
